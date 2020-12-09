f65873908daa04dc2133f5539b9386c95f8d7a91
Oleg Dramaretskyi <dramarec@gmail.com>
2020-12-09 20:27:38 +0200

    homeWork complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './styles.css';
+
+const body = document.querySelector(`body`);
+const btnStart = document.querySelector(`[data-action="start"]`);
+const btnStop = document.querySelector(`[data-action="stop"]`);
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+const min = 0;
+const max = colors.length;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const changeTheme = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      body.style.backgroundColor = colors[randomIntegerFromInterval(min, max)];
+    }, 1000);
+  },
+
+  stop() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+    body.style.backgroundColor = '';
+  },
+};
+
+btnStart.addEventListener('click', changeTheme.start.bind(changeTheme));
+btnStop.addEventListener('click', changeTheme.stop.bind(changeTheme));
